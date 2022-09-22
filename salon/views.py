9a78827d86df4ccdf3438198aae684a577b28c23
DOdrums <dirkornee@hotmail.com>
2022-09-22 15:04:17 +0000

    Begin work on HomePage view to display treatments

--- salon/views.py
+++ salon/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views import View
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from django.views.generic.list import ListView
+from .models import Treatment
+
 
 # Create your views here.
-class HomePage(View):
+class HomePage(ListView):
+    model = Treatment
     template_name = "index.html"
 
-    def get(self,request):
-        return render(request, self.template_name)     
-        
+    def get_treatment_data(self):
+        queryset = Treatment.objects.order_by('title')
+        treatments = queryset.objects.values()
+        return treatments
