908ea1d74a9fac246c65630cf261ba5c7faca3b5
DOdrums <dirkornee@hotmail.com>
2022-09-26 14:00:19 +0000

    finish basic footer styling

--- salon/views.py
+++ salon/views.py
@@ -7,6 +7,6 @@ from .models import Treatment
 class HomePage(View):
 
     def get(self, request):
-        queryset = list(Treatment.objects.order_by("title").values())
+        queryset = list(Treatment.objects.filter(display=True).order_by("title").values())
         treatments = {"treatments": queryset}
         return render(request, "index.html", context=treatments)
