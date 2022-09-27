ef7d6a16abae9d8548d6a30dbe77f8f8ae6ddb56
DOdrums <dirkornee@hotmail.com>
2022-09-27 12:45:48 +0000

    Add short_description to treatment model and style button on treatment card

--- salon/models.py
+++ salon/models.py
@@ -5,6 +5,7 @@ class Treatment(models.Model):
     appointment_type = models.CharField(max_length=150)
     title = models.CharField(max_length=150, unique=True)
     description = models.TextField()
+    short_description = models.CharField(max_length=225)
     image = CloudinaryField('image', default='placeholder')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     duration = models.DecimalField(max_digits=3, decimal_places=0)
