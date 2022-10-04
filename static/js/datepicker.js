817ee216b02396a8382e41e48353f618acd9bd7e
DOdrums <dirkornee@hotmail.com>
2022-10-04 14:51:43 +0000

    start setting up the datetimepicker

--- static/js/datepicker.js
+++ static/js/datepicker.js
@@ -1,5 +1,24 @@
 jQuery('#datepicker').datetimepicker({
     format:'d.m.Y H:i',
     inline:true,
-    lang:'en'
+    lang:'en',
+    step: 5,
+    todayButton: true,
+    allowTimes: [
+      '10',
+      '11',
+      '12',
+      '13',
+      '15',
+      '16',
+    ],
+    disabledWeekDays: [
+      0, 2, 3, 4, 6
+    ],
+  //   disabledDates: {
+  //   disabledDates: [
+  //     '27.10.2022'
+  //   ], formateDate: 'd.m.Y'
+  // },
+    
   });
