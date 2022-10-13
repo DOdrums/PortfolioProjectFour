372511e111ad4387f9f77e435524357278387a0c
DOdrums <dirkornee@hotmail.com>
2022-10-13 15:05:45 +0000

    Add onchange function to pass selected date in getBlockedTimesList function

--- static/js/datepicker.js
+++ static/js/datepicker.js
@@ -3,7 +3,10 @@ var disabledWeekDaysList = disabledWeekDays.map(function (x) {
   return parseInt(x, 10)
 })
 
-var allowTimes = [];
+dateToday = new Date()
+var allowTimesFinal = planningJS.allow_times.split(",")
+getBlockedTimesList(dateToday)
+console.log(allowTimesFinal)
 
 function getAllowdTimesList(date) {
   let allowedTimesList = []
@@ -17,16 +20,19 @@ function getAllowdTimesList(date) {
   return allowedTimesList
 }
 
-function getBlockedTimesList() {
+function getBlockedTimesList(selectedDate) {
   let allowedTimesList = getAllowdTimesList("2022-10-17T09:54:05+00:00")
 
   appointments.forEach(element => {
-    element.date_time = new Date(element.date_time);
-    let startTime = element.date_time
-    let endTime = new Date(element.date_time.getTime() + element.duration*60000)
-    for (time of allowedTimesList) {
-      if (startTime <= time && time < endTime) {
-        allowedTimesList = allowedTimesList.filter(e => e !== time);
+    appDate = new Date(element.date_time)
+
+    if (appDate.toDateString() === selectedDate.toDateString()) {
+      let startTime = appDate
+      let endTime = new Date(appDate.getTime() + element.duration*60000)
+      for (time of allowedTimesList) {
+        if (startTime <= time && time < endTime) {
+          allowedTimesList = allowedTimesList.filter(e => e !== time);
+        }
       }
     }
   });
@@ -35,22 +41,28 @@ function getBlockedTimesList() {
 }
 
 function convertBlockedTimesList(blockedTimesList) {
-  for (dateTime of blockedTimesList) {
-    let time = dateTime.getHours() + ":" + dateTime.getMinutes();
-    allowTimes.push(time)
-  }
+  allowTimesFinal.length = 0
+    for (dateTime of blockedTimesList) {
+      let time = dateTime.getHours() + ":" + dateTime.getMinutes();
+      allowTimesFinal.push(time)
+    }
+    console.log(allowTimesFinal)
 }
 
-getBlockedTimesList()
-
 jQuery('#datepicker').datetimepicker({
-  format:'d.m.Y H:i',
+  format:'c',
   inline:true,
   lang:'en',
-  step: 5,
   todayButton:true,
-  allowTimes: allowTimes, 
+  allowTimes: allowTimesFinal, 
   disabledWeekDays: disabledWeekDaysList,
   disabledDates: planningJS.disabled_dates.split(","), formatDate: 'd.m.Y',
+  onChangeDateTime: function(dp, $input) {
+    let selectedDate = new Date($input.val())
+    getBlockedTimesList(selectedDate)
+  }
   // Optionally add "allowDates: ["25.10.2022"], formatDate: 'd.m.Y'" , to allow only certain dates. For this to work, disabledDates and disabledWeekDays needs to be off/empty. 
 });
+
+// getBlockedTimesList()
+
