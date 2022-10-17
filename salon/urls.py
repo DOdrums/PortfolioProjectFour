054d60a975c222e2681e9fe46dfdeeae99a63125
DOdrums <dirkornee@hotmail.com>
2022-10-17 13:55:24 +0000

    Change alignement of book now button on homepage

--- salon/urls.py
+++ salon/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
-    path('', views.HomePage.as_view(), name="treatments-list"),
+    path('', views.HomePage.as_view(), name="home"),
     path('book', views.BookingModule.as_view(), name="book")
 ]
