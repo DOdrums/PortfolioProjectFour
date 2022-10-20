4eaf55f4a0cecf0a7d8d2ec3f47357320aa49f3d
DOdrums <dirkornee@hotmail.com>
2022-10-20 14:54:36 +0000

    Make email field on appointment non unique, so user can book multiple appointments

--- salon/models.py
+++ salon/models.py
@@ -20,7 +20,7 @@ class Treatment(models.Model):
 
 class Appointment(models.Model):
     user = models.ForeignKey(User, related_name="user", on_delete=models.PROTECT, null=True, blank=True)
-    email = models.EmailField(max_length=254, unique=True, blank=True)
+    email = models.EmailField(max_length=254, blank=True, null=True)
     first_name = models.CharField(max_length=254, null=True, blank=True)
     last_name = models.CharField(max_length=254, null=True, blank=True)
     phone_number = models.CharField(max_length=16, null=True, blank=True)
