927ab96fae430eb37a69eb1832f88744f493cfd1
DOdrums <dirkornee@hotmail.com>
2022-10-25 13:40:44 +0000

    Add custom-form css class to booking form and style book button

--- salon/forms.py
+++ salon/forms.py
@@ -5,15 +5,15 @@ class AppointmentForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(AppointmentForm, self).__init__(*args, **kwargs)
         self.fields['treatment_name'].queryset = self.fields['treatment_name'].queryset.exclude(active=False)
+        for fieldname, field in self.fields.items():
+            field.widget.attrs.update({
+            'class': 'custom-form-field'
+        })
+
 
     class Meta:
-        model = Appointment
-        
+        model = Appointment 
         fields = ('treatment_name', 'date_time', 'email', 'first_name', 'last_name', 'phone_number')
         labels = {
-            'treatment_name': ('Treatment'), 'date_time':   ('Date')
+            'treatment_name': ('Treatment'), 'date_time':   ('Date'), 'email': ('Email*'), 'first_name': ('First name*'), 'last_name': ('Last name*')
         }
-        # for fieldname, field in fields:
-        #     field.widget.attrs.update({
-        #     'class': 'custom-form-field'
-        # })
