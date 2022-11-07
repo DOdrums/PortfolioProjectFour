8943691b83e07b907e73a86cf327f63f347e4e0e
DOdrums <dirkornee@hotmail.com>
2022-11-07 13:09:45 +0000

    Deployment commit

--- nailsbyfaar/settings.py
+++ nailsbyfaar/settings.py
@@ -167,7 +167,7 @@ TIME_ZONE = 'Europe/Amsterdam'
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
