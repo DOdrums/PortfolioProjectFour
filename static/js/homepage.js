f827fb6d4ff3bc991793c49e54808c45707ae081
DOdrums <dirkornee@hotmail.com>
2022-11-11 09:01:06 +0000

    Add javascript to change hero-image margin-top value on collapse and show of mobile nav

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -1,9 +1,8 @@
-document.getElementById('navbar-toggler').addEventListener("click", collapsed)
-function collapsed() {
-    closed = $('#navbar-toggler').hasClass('collapsed')
-    if(closed === false) {
-    console.log("collapsed!")
-    } else {
-        console.log("JA!")
-    }
-}
+$('.navbar-collapse').on('hide.bs.collapse', function() {
+    $('.hero-image').css('margin-top', '-99px')
+});
+
+$('.navbar-collapse').on('show.bs.collapse', function() {
+    console.log("on showing");
+    $('.hero-image').css('margin-top', '0px')
+});
